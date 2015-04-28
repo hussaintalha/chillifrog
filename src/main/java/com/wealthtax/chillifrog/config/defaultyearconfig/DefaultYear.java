44e9531bf42b6aced98b57e9a6468d05b32c8b18
subhanalla <shaik.subhanalla@gmail.com>
2015-04-28 20:40:24 +0530

    added author name for testing.

--- src/main/java/com/wealthtax/chillifrog/config/defaultyearconfig/DefaultYear.java
+++ src/main/java/com/wealthtax/chillifrog/config/defaultyearconfig/DefaultYear.java
@@ -1,4 +1,8 @@
 package com.wealthtax.chillifrog.config.defaultyearconfig;
+/*
+ * author subhan
+ * 
+ */
 
 import java.util.Date;
 
@@ -8,18 +12,19 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
 @Entity
-@Table(name="occDefaultYear")
+@Table(name = "occDefaultYear")
 public class DefaultYear {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column
 	private int defaultYearID;
 	@Column
 	private String typeID;
 	@Column
 	private int defaultValue;
-	@Column	
+	@Column
 	private int preStartingYear;
 	@Column
 	private int preYearValue;
@@ -33,61 +38,79 @@ public class DefaultYear {
 	private int updaterID;
 	@Column
 	private Date updatedDate;
-	
+
 	public int getDefaultYearID() {
 		return defaultYearID;
 	}
+
 	public void setDefaultYearID(int defaultYearID) {
 		this.defaultYearID = defaultYearID;
 	}
+
 	public String getTypeID() {
 		return typeID;
 	}
+
 	public void setTypeID(String typeID) {
 		this.typeID = typeID;
 	}
+
 	public int getDefaultValue() {
 		return defaultValue;
 	}
+
 	public void setDefaultValue(int defaultValue) {
 		this.defaultValue = defaultValue;
 	}
+
 	public int getPreStartingYear() {
 		return preStartingYear;
 	}
+
 	public void setPreStartingYear(int preStartingYear) {
 		this.preStartingYear = preStartingYear;
 	}
+
 	public int getPreYearValue() {
 		return preYearValue;
 	}
+
 	public void setPreYearValue(int preYearValue) {
 		this.preYearValue = preYearValue;
 	}
+
 	public int getCountryID() {
 		return countryID;
 	}
+
 	public void setCountryID(int countryID) {
 		this.countryID = countryID;
 	}
+
 	public int getCreaterID() {
 		return createrID;
 	}
+
 	public void setCreaterID(int createrID) {
 		this.createrID = createrID;
 	}
+
 	public Date getCreatedDate() {
 		return createdDate;
 	}
+
 	public void setCreatedDate(Date createdDate) {
 		this.createdDate = createdDate;
 	}
+
 	public int getUpdaterID() {
 		return updaterID;
 	}
+
 	public void setUpdaterID(int updaterID) {
 		this.updaterID = updaterID;
 	}
+
 	@Override
 	public String toString() {
 		return "DefaultYear [defaultYearID=" + defaultYearID + ", typeID="
@@ -97,11 +120,13 @@ public class DefaultYear {
 				+ createrID + ", createdDate=" + createdDate + ", updaterID="
 				+ updaterID + ", updatedDate=" + updatedDate + "]";
 	}
+
 	public Date getUpdatedDate() {
 		return updatedDate;
 	}
+
 	public void setUpdatedDate(Date updatedDate) {
 		this.updatedDate = updatedDate;
 	}
-	
-} 
+
+}
